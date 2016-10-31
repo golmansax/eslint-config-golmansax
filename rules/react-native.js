8931f85c134efc0379268c46a9cadee28203dc09
Holman Gao <holman@golmansax.com>
2016-10-31 13:06:06 -0400

    Add no-alert rule for react-native

--- rules/react-native.js
+++ rules/react-native.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: ['react-native'],
   rules: {
+    'no-alert': 2, // Should use { Alert } from 'react-native'
     'react-native/no-unused-styles': 2,
     'react-native/split-platform-components': 2,
     'react-native/no-inline-styles': 2,
